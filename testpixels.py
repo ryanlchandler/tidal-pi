f7e507c259b5c01a3d8292502cbc562ce8309d48
Ryan Chandler <ryanchandler@Ryans-MacBook-Pro-2.local>
2018-09-01 10:04:09 -0400

    wip

--- testpixels.py
+++ testpixels.py
@@ -29,11 +29,11 @@ while True:
 	colorWipe(strip, Color(255, 0, 0))  # Red wipe
 	colorWipe(strip, Color(0, 255, 0))  # Blue wipe
 	colorWipe(strip, Color(0, 0, 255))  # Green wipe
-	# Theater chase animations.
-	theaterChase(strip, Color(127, 127, 127))  # White theater chase
-	theaterChase(strip, Color(127,   0,   0))  # Red theater chase
-	theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
-	# Rainbow animations.
-	rainbow(strip)
-	rainbowCycle(strip)
-	theaterChaseRainbow(strip)
+	# # Theater chase animations.
+	# theaterChase(strip, Color(127, 127, 127))  # White theater chase
+	# theaterChase(strip, Color(127,   0,   0))  # Red theater chase
+	# theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
+	# # Rainbow animations.
+	# rainbow(strip)
+	# rainbowCycle(strip)
+	# theaterChaseRainbow(strip)
