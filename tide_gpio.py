dc05ba8b1e2939985a8865ce96649464be4b0e4b
Ryan Chandler <ryanchandler@Ryans-MacBook-Pro-2.local>
2018-09-09 01:02:53 -0400

    wip

--- tide_gpio.py
+++ tide_gpio.py
@@ -56,9 +56,9 @@ LIGHT_9_IDX = 9
 
 
 
-OFF_COLOR = Color(0, 0, 0)
-IN_TIDE_COLOR = Color(3, 77, 203) # r g b
-OUT_TIDE_COLOR = Color(255, 0, 60)
+OFF_COLOR = Kolor(0, 0, 0)
+IN_TIDE_COLOR = Kolor(3, 77, 203) # r g b
+OUT_TIDE_COLOR = Kolor(255, 0, 60)
 
 # Create NeoPixel object with appropriate configuration.
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT)
@@ -211,8 +211,8 @@ def _signalOutLow():
     _turnOnLight(LIGHT_0_IDX, OUT_TIDE_COLOR)
     time.sleep(30)
 
-def _turnOnLight(lightIdx, color):
-    strip.setPixelColor(lightIdx, color)
+def _turnOnLight(lightIdx, color, brightness):
+    strip.setPixelColor(lightIdx, color["r"], color["g"], color["b"], brightness)
     strip.show()
 
 def _turnOffAllLights(leaveOnStart, leaveOnEnd):
@@ -231,14 +231,26 @@ def _turnOffAllLights(leaveOnStart, leaveOnEnd):
     # _turnOffLight(LIGHT_9_IDX)
 
 def _turnOffLight(lightIdx):
-    strip.setPixelColor(lightIdx, OFF_COLOR)
+    strip.setPixelColor(lightIdx, OFF_COLOR["r"], OFF_COLOR["g"], OFF_COLOR["b"], 0)
     strip.show()
+
+def Kolor(r, g, b):
+    return {
+        "r": r,
+        "g": g,
+        "b": b
+    }
 
 def _flashLight(lightIdx, color):
     for i in range(20):
-        print(color)
-
-        _turnOnLight(lightIdx, color)
+        _turnOnLight(lightIdx, color, 255)
         time.sleep(1)
         # _turnOffLight(lightIdx)
-        time.sleep(.5)
+        _turnOnLight(lightIdx, color, 200)
+        time.sleep(.2)
+        _turnOnLight(lightIdx, color, 150)
+        time.sleep(.2)
+        _turnOnLight(lightIdx, color, 100)
+        time.sleep(.2)
+        _turnOnLight(lightIdx, color, 50)
+        time.sleep(.2)
