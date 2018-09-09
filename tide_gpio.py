1eb2fd8cb0a47047bf3af1826141e73cd3be8046
Ryan Chandler <ryanchandler@Ryans-MacBook-Pro-2.local>
2018-09-09 00:29:05 -0400

    wip

--- tide_gpio.py
+++ tide_gpio.py
@@ -58,7 +58,7 @@ LIGHT_9_IDX = 9
 
 OFF_COLOR = Color(0, 0, 0)
 IN_TIDE_COLOR = Color(3, 77, 203) # r g b
-OUT_TIDE_COLOR = Color(255, 70, 119)
+OUT_TIDE_COLOR = Color(255, 43, 100)
 
 # Create NeoPixel object with appropriate configuration.
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT)
