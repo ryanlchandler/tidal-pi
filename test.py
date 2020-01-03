296c081a604d457788885b2a5c1fe839591870d3
Ryan Chandler <ryanchandler@Ryans-MacBook-Pro-2.local>
2020-01-03 07:38:17 -0500

    wip

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from tide_gpio import *
 
 threads = []
 threads.append(runForecastUpdateJob())
-# threads.append(runClockUpdateJob())
+threads.append(runClockUpdateJob())
 # threads.append(runTideLightUpdateJob())
 
 for thread in threads:
