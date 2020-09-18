b1df99aa526ad964991b6ebed546552054ec232d
Ryan Chandler <ryanlchandler@gmail.com>
2020-09-18 16:26:01 -0400

    wip

--- scripts/tidal-pi.sh
+++ scripts/tidal-pi.sh
@@ -14,10 +14,10 @@ CURRENT_HASH=$(git -C /home/pi/git/tidal-pi rev-parse HEAD)
 PREV_HASH=`cat /home/pi/tidal-pi-hash`
 git -C /home/pi/git/tidal-pi rev-parse HEAD > /home/pi/tidal-pi-hash
 
-echo "$PREV_HASH == $CURRENT_HASH"
 if ["$PREV_HASH" == "$CURRENT_HASH"]; then
   ./run.sh
 else
+  echo "$PREV_HASH != $CURRENT_HASH"
   ./build.sh
   ./run.sh
 fi
