fdf84bf8f4adbf3690b6902899695bf84a775aa7
Ryan Chandler <ryanlchandler@gmail.com>
2020-09-18 15:42:57 -0400

    wip

--- scripts/tidal-pi.sh
+++ scripts/tidal-pi.sh
@@ -4,6 +4,8 @@
 until ping -c1 github.com &>/dev/null; do :; done
 rm -rf /home/pi/git/tidal-pi
 git clone https://github.com/ryanlchandler/tidal-pi.git /home/pi/git/tidal-pi
+git -C /home/pi/git/tidal-pi submodule init \
+git -C /home/pi/git/tidal-pi submodule update
 cp /home/pi/git/tidal-pi/scripts/wifi-config.sh /home/pi/scripts/wifi-config.sh
 cp /home/pi/git/tidal-pi/scripts/tidal-pi.sh /home/pi/scripts/tidal-pi.sh
 cd /home/pi/git/tidal-pi
