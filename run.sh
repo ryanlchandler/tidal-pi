a780c5bfd5b9d327b9c4caaa878135be5c0b634f
Ryan Chandler <ryanlchandler@gmail.com>
2020-09-18 15:58:22 -0400

    wip

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+STRIP_TYPE="${STRIP_TYPE:-LoggerStrip}"
+
 docker build --tag tidalpi:1.0 .
 docker rm --force tidalpi
-docker run -v /var/log/pi:/app/log --privileged --detach --name tidalpi tidalpi:1.0
+docker run -v /var/log/pi:/app/log -v /var/data:/app/data --privileged --env STRIP_TYPE=$STRIP_TYPE --detach --name tidalpi tidalpi:1.0
--- tidal_pi/config.py
+++ tidal_pi/config.py
@@ -2,7 +2,7 @@ import os
 
 TIDE_URL = "https://tidesandcurrents.noaa.gov/api/datagetter"
 STATION_ID = "8679083"
-FORECAST_FILE = "./forecast.json"
+FORECAST_FILE = "/app/data/forecast.json"
 LOW_TIDE_PIN = 16
 HIGH_TIDE_PIN = 12
 STRIP_TYPE = os.getenv('STRIP_TYPE', "LoggerStrip")
