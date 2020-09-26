4b27c4a068c40077134dd912044bb18e3c86945f
Ryan Chandler <ryanlchandler@gmail.com>
2020-09-26 10:24:04 -0400

    wip

--- run.sh
+++ run.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-STRIP_TYPE="${STRIP_TYPE:-LoggerStrip}"
+LIGHT_STRIP_TYPE="${LIGHT_STRIP_TYPE:-LoggerStrip}"
 WEATHER_SERVICE="${WEATHER_SERVICE:-LoggerWeatherService}"
 CLOCK_TYPE="${CLOCK_TYPE:-LoggerClock}"
 LOG_LEVEL="${LOG_LEVEL:-INFO}"
 
-echo "STRIP_TYPE=$STRIP_TYPE"
-
 docker rm --force tidalpi
+
 docker run \
 -v /var/log/pi:/app/log \
 -v /var/data:/app/data \
 --privileged \
---env STRIP_TYPE=$STRIP_TYPE \
+--env LIGHT_STRIP_TYPE=$LIGHT_STRIP_TYPE \
 --env WEATHER_SERVICE=$WEATHER_SERVICE \
 --env CLOCK_TYPE=$CLOCK_TYPE \
 --env LOG_LEVEL=$LOG_LEVEL \
