76bca7dd35b491167ffa71c7d1684a6678e0081a
Ryan Chandler <ryanlchandler@gmail.com>
2020-09-26 16:32:04 -0400

    wip

--- scripts/tidal-pi.sh
+++ scripts/tidal-pi.sh
@@ -5,7 +5,7 @@ until ping -c1 github.com &>/dev/null; do :; done
 if [ -d "/home/pi/git/tidal-pi" ]
 then
     git -C /home/pi/git/tidal-pi stash
-    git -C /home/pi/git/tidal-pi checkout origin/master
+    git -C /home/pi/git/tidal-pi pull https://github.com/ryanlchandler/tidal-pi.git
 else
     git clone https://github.com/ryanlchandler/tidal-pi.git /home/pi/git/tidal-pi
     git -C /home/pi/git/tidal-pi submodule init
@@ -25,7 +25,7 @@ export WEATHER_SERVICE=NoaaWeatherService
 export LIGHT_STRIP_TYPE=NeoPixelStrip
 export CLOCK_TYPE=LedClock
 
-if ["$PREV_HASH" == "$CURRENT_HASH"]; then
+if ["$PREV_HASH" == "$CURRENT_HASH"] then
   ./run.sh
 else
   echo "$PREV_HASH != $CURRENT_HASH"
