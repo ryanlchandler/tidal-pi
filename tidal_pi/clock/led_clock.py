ee26637e16ac2a4ce5b43f8a2b3452e4aae3134d
Ryan Chandler <ryanlchandler@gmail.com>
2020-09-26 17:48:17 -0400

    wip

--- tidal_pi/clock/led_clock.py
+++ tidal_pi/clock/led_clock.py
@@ -1,3 +1,4 @@
+import sys
 import logging
 from Adafruit_LED_Backpack import SevenSegment
 
@@ -10,20 +11,23 @@ class LedClock:
         logging.debug("new clock named {} for address {}".format(name, address))
 
     def set_clock(self, tide):
-        logging.debug("set clock {} to {}".format(self.name, tide.get_date_time()))
-        tideTime = tide.get_date_time()
-        hour = tideTime.hour
-        minute = tideTime.minute
+        try:
+            logging.debug("set clock {} to {}".format(self.name, tide.get_date_time()))
+            tideTime = tide.get_date_time()
+            hour = tideTime.hour
+            minute = tideTime.minute
 
-        self.clock.clear()
-        # Set hours
-        self.clock.set_digit(0, int(hour / 10))  # Tens
-        self.clock.set_digit(1, hour % 10)  # Ones
+            self.clock.clear()
+            # Set hours
+            self.clock.set_digit(0, int(hour / 10))  # Tens
+            self.clock.set_digit(1, hour % 10)  # Ones
 
-        # Set minutes
-        self.clock.set_digit(2, int(minute / 10))  # Tens
-        self.clock.set_digit(3, minute % 10)  # Ones
+            # Set minutes
+            self.clock.set_digit(2, int(minute / 10))  # Tens
+            self.clock.set_digit(3, minute % 10)  # Ones
 
-        self.clock.set_colon(1)  # colon
+            self.clock.set_colon(1)  # colon
 
-        self.clock.write_display()
+            self.clock.write_display()
+        except:
+            logging.error("could not set clock", sys.exc_info()[0])
