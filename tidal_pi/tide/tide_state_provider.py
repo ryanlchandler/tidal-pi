d67d9ab96d50b0bb376a6ee6dde704ac7e294330
Ryan Chandler <ryanlchandler@gmail.com>
2020-09-26 23:21:43 -0400

    wip

--- tidal_pi/tide/tide_state_provider.py
+++ tidal_pi/tide/tide_state_provider.py
@@ -1,7 +1,19 @@
+import logging
+
 class TideStateProvider():
 
     def __init__(self, tide_chart):
         self.tide_chart = tide_chart
 
     def get_tide_state(self):
-        return self.tide_chart.get_tide_state()
+        ts = self.tide_chart.get_tide_state()
+        logging.debug("providing tide state: \n   previous_tide: {}\n   next_tide: {}\n    next_high_tide: {}\n    next_low_tide: {}\n   current_tide_level: {} - {}"
+                    .format(
+                        ts.get_previous_tide().get_date_time_str(),
+                        ts.get_next_tide().get_date_time_str(),
+                        ts.get_next_high_tide().get_date_time_str(),
+                        ts.get_next_low_tide().get_date_time_str(),
+                        ts.get_current_tide_level().get_percent_of_high_tide(),
+                        ts.get_current_tide_level().get_tide_type()
+                    ))
+        return ts
