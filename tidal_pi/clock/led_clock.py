c20effe01cece37c7e6d4e7a7d38a398da33ef48
Ryan Chandler <ryanlchandler@gmail.com>
2020-09-26 23:11:01 -0400

    wip

--- tidal_pi/clock/led_clock.py
+++ tidal_pi/clock/led_clock.py
@@ -12,25 +12,29 @@ class LedClock:
             logging.debug("new clock named {} for address {}".format(name, address))
         except:
             logging.error("could not start clock", sys.exc_info()[0])
+            self.clock = None
 
     def set_clock(self, tide):
-        try:
-            logging.debug("set clock {} to {}".format(self.name, tide.get_date_time()))
-            tideTime = tide.get_date_time()
-            hour = tideTime.hour
-            minute = tideTime.minute
+        if self.clock != None:
+            try:
+                logging.debug("set clock {} to {}".format(self.name, tide.get_date_time()))
+                tideTime = tide.get_date_time()
+                hour = tideTime.hour
+                minute = tideTime.minute
 
-            self.clock.clear()
-            # Set hours
-            self.clock.set_digit(0, int(hour / 10))  # Tens
-            self.clock.set_digit(1, hour % 10)  # Ones
+                self.clock.clear()
+                # Set hours
+                self.clock.set_digit(0, int(hour / 10))  # Tens
+                self.clock.set_digit(1, hour % 10)  # Ones
 
-            # Set minutes
-            self.clock.set_digit(2, int(minute / 10))  # Tens
-            self.clock.set_digit(3, minute % 10)  # Ones
+                # Set minutes
+                self.clock.set_digit(2, int(minute / 10))  # Tens
+                self.clock.set_digit(3, minute % 10)  # Ones
 
-            self.clock.set_colon(1)  # colon
+                self.clock.set_colon(1)  # colon
 
-            self.clock.write_display()
-        except:
-            logging.error("could not set clock", sys.exc_info()[0])
+                self.clock.write_display()
+            except:
+                logging.error("could not set clock", sys.exc_info()[0])
+        else:
+            logging.info("clock did not start cannot set {}".format(self.name))
