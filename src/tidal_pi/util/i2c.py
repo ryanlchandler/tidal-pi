01b00ed6708e2e05875e4d2c7585a61b804e2de5
Ryan Chandler <ryanlchandler@gmail.com>
2020-10-16 13:55:40 -0400

    wip

--- src/tidal_pi/util/i2c.py
+++ src/tidal_pi/util/i2c.py
@@ -6,8 +6,11 @@ logger = logging.getLogger(__name__)
 
 def i2cdetect():
     try:
-        result = subprocess.run(['i2cdetect', '-y 1'], stdout=subprocess.PIPE)
-        return result.stdout
+        result = subprocess.run(["i2cdetect", "-y", "1"], capture_output=True)
+        if result.returncode == 0:
+            return result.stdout
+        else:
+            return result.stderr
     except:
         logger.error("error running i2cdetect", sys.exc_info()[0])
         return "error running i2cdetect"
