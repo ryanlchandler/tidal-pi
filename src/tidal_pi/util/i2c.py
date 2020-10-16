4a8a1e8e47210d1785f8c134cab089976f390905
Ryan Chandler <ryanlchandler@gmail.com>
2020-10-16 14:58:52 -0400

    wip

--- src/tidal_pi/util/i2c.py
+++ src/tidal_pi/util/i2c.py
@@ -8,9 +8,9 @@ def i2cdetect():
     try:
         result = subprocess.run(["/usr/sbin/i2cdetect -y 1"], capture_output=True, shell=True)
         if result.returncode == 0:
-            return result.stdout
+            return result.stdout.decode("utf-8")
         else:
-            return result.stderr
+            return result.stderr.decode("utf-8")
     except:
         logger.error("error running i2cdetect", sys.exc_info()[0])
         return "error running i2cdetect"
