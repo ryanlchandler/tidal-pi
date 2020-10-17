9be235186d078e1d80ae8e18c0dbf7490b7eb6c5
Ryan Chandler <ryanlchandler@gmail.com>
2020-10-17 18:19:43 -0400

    wip

--- src/tidal_pi/clock/clock_job.py
+++ src/tidal_pi/clock/clock_job.py
@@ -6,8 +6,8 @@ import time
 class ClockJob:
 
     def __init__(self, tide_state_provider):
-        self.highTideClock = clock_factory.create_clock("HighTideClock", config.HIGH_TIDE_CLOCK_ADDRESS)
-        # self.lowTideClock = clock_factory.create_clock("LowTideClock", config.LOW_TIDE_CLOCK_ADDRESS)
+        # self.highTideClock = clock_factory.create_clock("HighTideClock", config.HIGH_TIDE_CLOCK_ADDRESS)
+        self.lowTideClock = clock_factory.create_clock("LowTideClock", config.LOW_TIDE_CLOCK_ADDRESS)
         self.tide_state_provider = tide_state_provider
 
     def run(self):
@@ -15,5 +15,5 @@ class ClockJob:
             tide_state = self.tide_state_provider.get_tide_state()
 
             if tide_state != None:
-                self.highTideClock.set_clock(tide_state.get_next_high_tide())
-                # self.lowTideClock.set_clock(tide_state.get_next_low_tide())
+                # self.highTideClock.set_clock(tide_state.get_next_high_tide())
+                self.lowTideClock.set_clock(tide_state.get_next_low_tide())
